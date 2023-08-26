3d2c93743e8452a5516699cb9eb30dcdf74d304c
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-08-26 20:23:14 +0300

    home page - show all files dunctionality

--- controllers/home.js
+++ controllers/home.js
@@ -4,18 +4,18 @@ const path = require('path');
 module.exports.getDirectory = (req, res, next) => {
 
     const folders = [];
+    const otherTypes = [];
 
     (async function () {
         const files = await fs.readdir(path.join(__dirname, '../../'));
     
         for (let f of files) {
             const stat = await fs.lstat(path.join(__dirname, '../../',f));
-            if (stat.isDirectory()) {
-                folders.push(f);
-            }
+            if (stat.isDirectory()) folders.push(f);
+            else otherTypes.push(f);
         }
         res.render('home', {
-            folders
+            folders, otherTypes
         });
     })();
 
