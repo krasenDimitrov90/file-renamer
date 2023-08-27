e845117bf9a7ca88a0423423b17c631c1ac20666
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-08-27 15:21:45 +0300

    remake folders routs to use only get requests

--- routes/folders.js
+++ routes/folders.js
@@ -1,8 +1,8 @@
 const foldersController = require('../controllers/folders');
 const router = require('express').Router();
 
-router.post(/^(.*)$/, foldersController.getDirectory);
-router.get('/', foldersController.getDirectory);
+router.get(/^(.*)$/, foldersController.getDirectory);
+// router.get('/', foldersController.getDirectory);
 
 module.exports = router;
 
